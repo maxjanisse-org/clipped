5770f5382eb78913e3f14ad1d1f3b3f8b5dced80
Max Janisse <maxjanisse@gmail.com>
2026-04-05 17:47:47 -0700

    Rewrote script to better handle each use case.

--- env-setup.bash
+++ env-setup.bash
@@ -1,6 +1,16 @@
 #!/bin/bash
 # Max Janisse - 2026
 
-python3 -m venv venv
-source venv/bin/activate
-pip --install scipy sounddevice
+if [ ! -d "venv" ]; then
+    echo -e "Creating Python environment..."
+    python3 -m venv venv > /dev/null
+    echo -e "Activating Python environment..."
+    source venv/bin/activate
+    echo -e "Installing packages...\n"
+    pip install scipy sounddevice > /dev/null
+elif [[ -z "$VIRTUAL_ENV" ]]; then
+    echo -e "Activating existing Python environment..."
+    source venv/bin/activate
+else
+    echo -e "Python environment exists and is active, no action necessary..."
+fi
