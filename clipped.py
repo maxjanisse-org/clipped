a6f54f333566c9bd807e699d390571922ce06cd7
Max Janisse <maxjanisse@gmail.com>
2026-04-07 11:35:35 -0700

    Finished function comments. Refactored arguments/comments/variable names to use 'scale' instead of 'factor'. Refactored validation of 'scale' arguments.'

--- clipped.py
+++ clipped.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# Author: Max Janisse
 import argparse
 import numpy as np
 from scipy.io.wavfile import write
@@ -10,29 +11,42 @@ clipped_filename = "clipped.wav"
 
 amp = np.iinfo(np.int16)
 
-def clip(ratio):
+def validate_scale(value):
     """
-    Curried function
+    Validation function used by the ArgumentParser to ensure the value is a float and is between 0 and 1
 
     Args:
-        ratio (float): The decimal value to clip the sample size by.
+        value (Unknown): The value provided by the user through the command-line
+    
+    Returns:
+        The validated float value to use
+    
+    Raises:
+        ValueError: When argument is not a float value or if it is not between 0 and 1
+    """
+    scale = float(value)
+    if scale <= 0 or scale > 1:
+        raise ValueError(f"*** invalid scale {scale}, should be between 0 and 1")
+    return scale
+
+def clip(scale):
+    """
+    Function that provides a function that can be used as a filter
+
+    Args:
+        scale (float): The decimal value to clip the sample size by.
 
     Returns:
         Function used to filter 
-
-    Raises:
-        Exception: If ratio <= 0 or ratio > 1
     """
-    if ratio <= 0 or ratio > 1:
-        raise Exception(f"*** invalid ratio {ratio}, should be between 0 and 1")
-    max = amp.max*ratio
-    min = amp.min*ratio
+    max = amp.max*scale
+    min = amp.min*scale
     def f(value):
         """
         Nested function used to map 
 
         Args:
-            value (float): The value 
+            value (float): The value to evaluate
         
         Returns:
             Float value
@@ -44,7 +58,7 @@ def clip(ratio):
 
 def sine_wave(freq, samples, max_amp):
     """
-    Set up a Sine Wave
+    Set up a Sine wave
 
     Args:
         freq (float): The frequency
@@ -52,50 +66,53 @@ def sine_wave(freq, samples, max_amp):
         max_amp (int): The maximum amplitude
 
     Returns:
-        Function 
+        Function that takes a scale value as an argument and generates a sine wave
     """
-    return lambda factor: np.array((max_amp * factor) * np.sin(2. * np.pi * freq * samples)).astype(np.int16)
+    return lambda scale: np.array((max_amp * scale) * np.sin(2. * np.pi * freq * samples)).astype(np.int16)
 
-def clipped_sine_data(factor):
+def clipped_sine_data(scale):
     """
-
+    Set up a Sine wave where the amplitudes will be clipped if they exceed the threshold defined by the `scale` argument
 
     Args:
-        factor (float): The decimal value to limit
+        scale (float): The decimal value used to determine the clipping threshold
 
     Returns:
-        Function
+        Function that takes in the base sine wave data and returns the clipped form of the data
     """
-    return lambda data: np.array(list(map(clip(factor), data))).astype(np.int16)
+    return lambda data: np.array(list(map(clip(scale), data))).astype(np.int16)
 
 def main(args):
     freq = args.frequency
+    sine_amp_scale = args.sine_amp_scale
+    clip_amp_scale = args.clip_amp_scale
+    clip_threshold = args.clip_threshold
     samplerate = args.samplerate
     samples = np.linspace(0., 1., samplerate)
 
-    set_max_amp = sine_wave(freq, samples, amp.max)
+    set_amp_scale = sine_wave(freq, samples, amp.max)
 
     print(f"Generated {freq}Hz @ {samplerate} samples/second sine wave")
 
-    sine_wave_data = set_max_amp(0.25)
+    sine_wave_data = set_amp_scale(sine_amp_scale)
 
     if args.verbose:
-        print(f"  -> Amplitude range {math.ceil(amp.min*0.25)}..{math.ceil(amp.max*0.25)}")
+        print(f"  -> Amplitude range {math.ceil(amp.min*sine_amp_scale)}..{math.ceil(amp.max*sine_amp_scale)}")
     
     write(sine_filename, samplerate, sine_wave_data)
-    print(f"Generated sine signal in file: {sine_filename}...")
+    print(f"Wrote sine signal to file '{sine_filename}'")
     
-    sine_wave_data = set_max_amp(0.5)
-    clip_sine_wave = clipped_sine_data(0.25)
+    sine_wave_data = set_amp_scale(clip_amp_scale)
+    clip_sine_wave = clipped_sine_data(clip_threshold)
 
     if args.verbose:
-        print(f"  -> Amplitude range {math.ceil(amp.min*0.5)}..{math.ceil(amp.max*0.5)}")
-        print(f"  -> Signal clipped at {math.ceil(amp.min*0.25)}..{math.ceil(amp.max*0.25)}")
+        print(f"  -> Amplitude range {math.ceil(amp.min*clip_amp_scale)}..{math.ceil(amp.max*clip_amp_scale)}")
+        print(f"  -> Signal clipped at {math.ceil(amp.min*clip_threshold)}..{math.ceil(amp.max*clip_threshold)}")
     
     clipped_wave_data = clip_sine_wave(sine_wave_data)
 
     write(clipped_filename, samplerate, clipped_wave_data)
-    print(f"Generated clipped signal in file: {clipped_filename}...")
+    print(f"Wrote clipped signal to file '{clipped_filename}'")
 
     if not args.quiet:
         print("Playing clipped signal...")
@@ -105,14 +122,16 @@ def main(args):
     else:
         print("Playback skipped, exiting...")
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="clipped.py",
         usage="python3 %(prog)s [options]"
     )
     parser.add_argument("-q", "--quiet", action="store_true", help="Prevent the clipped audio from playing.")
-    parser.add_argument("-f", "--frequency", default=440, type=int, help="Set the frequency.")
-    parser.add_argument("-s", "--samplerate", default=48000, type=int, help="Set the samplerate.")
+    parser.add_argument("-f", "--frequency", default=440, type=int, help="Set the frequency. [default: %(default)s]")
+    parser.add_argument("-s", "--samplerate", default=48000, type=int, help="Set the samplerate. [default: %(default)s]")
     parser.add_argument("-v", "--verbose", action="store_true", help="Print more information about the generated audio.")
+    parser.add_argument("--sine-amp-scale", default=0.25, type=validate_scale, help="Scale the maximum amplitude. Value must be between 0 and 1. [default: %(default)s]")
+    parser.add_argument("--clip-amp-scale", default=0.5, type=validate_scale, help="Scale the maximum amplitude for the clipped audio. Value must be between 0 and 1. [default: %(default)s]")
+    parser.add_argument("--clip-threshold", default=0.25, type=validate_scale, help="Set the maximum threshold for the clipped audio. Value must be between 0 and 1. [default: %(default)s]")
     main(parser.parse_args())
